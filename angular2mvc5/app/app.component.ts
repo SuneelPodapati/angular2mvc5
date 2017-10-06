806877bbacd504c5a8f30e26df8041701f823abe
suneel.podapati <suneel.podapati@HDC4-D-7NYN6C2>
2017-10-06 18:26:24 +0530

    Angular 2 checkin

---
+++ angular2mvc5/app/app.component.ts
@@ -0,0 +1,11 @@
+﻿import { Component } from '@angular/core';
+
+@Component({
+    selector: 'my-app',
+    template: `<div style="border:dashed; background-color:grey">
+    <h4>
+        This is a Angular 2 Component
+    </h4>
+</div>`
+})
+export class AppComponent { }
---
+++ angular2mvc5/app/main.ts
@@ -0,0 +1,13 @@
+﻿import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app.module';
+import { Operator } from 'rxjs/Operator';
+import { Observable } from 'rxjs/Observable';
+
+declare module 'rxjs/Subject' {
+    interface Subject<T> {
+        lift<R>(operator: Operator<T, R>): Observable<R>;
+    }
+}
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
